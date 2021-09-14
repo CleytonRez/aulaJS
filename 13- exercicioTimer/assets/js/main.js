9d014b6f8ed19d4a2d6ebc6522b4bf49339c4a79
Cleyton Eduardo Rezende <cleytonmgrezende@gmail.com>
2021-09-14 18:24:12 -0300

    exercicioTimer

--- 13- exercicioTimer/assets/js/main.js
+++ 13- exercicioTimer/assets/js/main.js
@@ -1,70 +1,41 @@
-const form = document.querySelector('#form');
-
-form.addEventListener('submit', function (e) {
-    e.preventDefault();
-    const inputPeso = e.target.querySelector('#peso')
-    const inputAltura = e.target.querySelector('#altura')
-
-    const peso = Number(inputPeso.value);
-    const altura = Number(inputAltura.value)
-
-    if (!peso) {
-        setResultado('Peso inválido!', false);
-        return;
+function relogio() {
+    function criaHoraDosSegundos(segundos) {
+        const data = new Date(segundos * 1000);
+        return data.toLocaleTimeString('pt-BR', {
+            hour12: false,
+            timeZone: 'UTC'
+        })
     }
 
-    if (!altura) {
-        setResultado('Altura inválida!', false);
-        return;
+    const relogio = document.querySelector('.relogio');
+    let segundos = 0;
+    let timer;
+
+    function iniciaRelogio() {
+        timer = setInterval(function () {
+            segundos++;
+            relogio.innerHTML = criaHoraDosSegundos(segundos);
+        }, 1000);
     }
 
-    const imc = getImc(peso, altura);
-    const nivelImc = getNivelImc(imc)
+    document.addEventListener('click', function (e) {
+        const el = e.target;
 
-    const msg = `Seu IMC é ${imc} (${nivelImc})`;
-
-    setResultado(msg, true);
-
-
-});
-
-function getNivelImc(imc) {
-    const nivel = ['Abaixo do peso', 'Peso normal', 'Sobrepeso',
-        'Obesidade grau 1', 'Obesidade grau 2', 'Obesidade grau 3'];
-
-    if (imc >= 39.9) return nivel[5];
-    if (imc >= 34.9) return nivel[4];
-    if (imc >= 29.9) return nivel[3];
-    if (imc >= 24.9) return nivel[2];
-    if (imc >= 18.5) return nivel[1];
-    if (imc < 34.9) return nivel[0];
+        if (el.classList.contains('zerar')) {
+            clearInterval(timer);
+            relogio.innerHtML = '00:00:00';
+            relogio.classList.remove('pausado');
+            segundos = 0;
+        }
+        if (el.classList.contains('iniciar')) {
+            relogio.classList.remove('pausado');
+            clearInterval(timer);
+            iniciaRelogio();
+        }
+        if (el.classList.contains('pausar')) {
+            clearInterval(timer);
+            relogio.classList.add('pausado');
+        }
+    })
 }
-
-function getImc(peso, altura) {
-    const imc = peso / altura ** 2;
-    return imc.toFixed(2);
-}
-
-function criaP() {
-    const p = document.createElement('p');
-    return p
-}
-
-function setResultado(msg, isValid) {
-    const resultado = document.querySelector('#resultado');
-    resultado.innerHTML = '';
-
-
-    const p = criaP();
-
-    if (isValid) {
-        p.classList.add('paragrafo-resultado');
-
-    } else {
-        p.classList.add('bad');
-    }
-
-
-    p.innerHTML = msg;
-    resultado.appendChild(p);
-}
+relogio();
